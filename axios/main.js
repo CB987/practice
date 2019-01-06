c46c1b143be34ce050c7d78391dd4152b26885d7
Clare Barton <grimalkinclare@gmail.com>
2019-01-06 17:20:16 -0500

    adds response functions

--- axios/main.js
+++ axios/main.js
@@ -11,6 +11,26 @@ function performGetRequest1() {
         });
 }
 
+function generateSuccessHTMLOutput(response) {
+    return '<h4>Result:</h4>' +
+        '<h5>Status:</h5>' +
+        '<pre>' + response.status + '' + response.statusText + '</pre>' +
+        '<h5>Headers:</h5>' +
+        '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
+        '<h5>Data:</h5>' + '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+}
+
+function generateErrorHTMLOutput(error) {
+    return '<h4>Result:</h4>' +
+        '<h5>Message:</h5>' +
+        '<pre>' + error.message + '</pre>' +
+        '<h5>Status:</h5>' +
+        '<pre>' + error.response.status + '' + error.response.statusText + '</pre>' +
+        '<h5>Headers:</h5>' +
+        '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>' +
+        '<h5>Data:</h5>' + '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+}
+
 function performGetRequest2() {
     var resultElement = document.getElementById('getResult2');
     var todoId = document.getElementById('todoId').nodeValue;
@@ -39,7 +59,7 @@ function performPostRequest(e) {
     var todoTitle = document.getElementById('todoTitle').nodeValue;
     resultElement.innerHTML = '';
 
-    axios.post('http://jsonplaceholder.typicode.com/todos' {
+    axios.post('http://jsonplaceholder.typicode.com/todos', {
         userId: '1',
         title: todoTitle,
         completed: false
